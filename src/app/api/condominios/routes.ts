655d1851c28a00d79b37e07f2ec49ca2a6041a36
Tiago <rstiagors@gmail.com>
2025-09-19 22:01:44 -0300

    Get do supebase

---
+++ src/app/api/condominios/routes.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import { getCondominios } from "@/services/condominio.services";
+
+export async function GET() {
+    try {
+        const data = await getCondominios();
+
+        return NextResponse.json({
+            sucess: true,
+            count: data.length,
+            data,
+        }, { status: 200});
+    } catch (e: any) {
+        return NextResponse.json({
+            sucess: false,
+            error: e.message ?? "Erro inesperado",
+        }, { status: 400 });
+    }
+}
